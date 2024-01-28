5796222339d687dea8a8f2caeb6d56038f1523e6
rahasin <raha.sajadifar83@sharif.edu>
2024-01-28 18:50:59 +0330

    ficing clinic pharmacy appointment

--- APProjectPhase2/ClinicAppointmentPharmacy/apps.py
+++ APProjectPhase2/ClinicAppointmentPharmacy/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppnameConfig(AppConfig):
+class ClinicAppointmentPharmacyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appname'
+    name = 'ClinicAppointmentPharmacy'
