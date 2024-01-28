8aeb79e51825640d5fa497bf136aa60a9ee06f3e
Pardis <rahiminasrpardis@gmail.com>
2024-01-28 17:34:03 +0330

    urls changed

--- APProjectPhase2/Patient/urls.py
+++ APProjectPhase2/Patient/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from Patient import views
+from Patient.views import sign_up,log_in,log_out,select_clinic_capacity_info,select_patient_reserved_appointments_info
 
 urlpatterns = [
-    path('signup/', views.sign_up, name='signup'),
-    path('login/', views.log_in, name='login'),
-    path('logout/', views.log_out, name='logout'),
-    path('clinic/<int:clinic_id>/', views.select_clinic_capacity_info, name='clinic_info'),
-    path('patient/<str:patient_national_code>/', views.select_patient_reserved_appointments_info, name='patient_info'),
+    path('signup/', sign_up, name='signup'),
+    path('login/', log_in, name='login'),
+    path('logout/', log_out, name='logout'),
+    path('clinic/<int:clinic_id>/', select_clinic_capacity_info, name='clinic_info'),
+    path('patient/<str:patient_national_code>/', select_patient_reserved_appointments_info, name='patient_info'),
 ]
