5a46914c3627adc0d69602017c23be784946306b
shidehh <154590082+shidehh@users.noreply.github.com>
2024-01-30 22:11:48 +0330

    urls of doctor

--- APProjectPhase2/AppointmentPharmacy/urls.py
+++ APProjectPhase2/AppointmentPharmacy/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('reserve_appointment/', reserve_appointment, name='reserve_appointment'),
     path('cancel_appointment/', cancel_appointment, name='cancel_appointment'),
     path('increase_capacity/', increase_capacity, name='increase_capacity'),
-    path('check_patient/<str:patient_national_code>/', check_patient, name='check_patient'),
+    path('pharmacy/', views.pharmacy_view, name='pharmacy_view'),
 ]
